3c1af943452e1441ac77524eac5224499d79c3aa
Ebuka <ebuka2264@yahoo.com>
2023-09-25 22:59:46 +0100

    qvdao:init

---
+++ script/DeployQvDao.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+import {Script, console2} from "forge-std/Script.sol";
+
+
+contract DeployQvDao is Script {
+
+    function run() public {
+        vm.broadcast();
+    }
+
+}
